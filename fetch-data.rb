2b11006c46354432474c52108c52eb1411d6bcb0
Matt Zimmerman <mdz@heroku.com>
2012-11-08 15:51:58 -0800

    Rip out email stuff

--- fetch-data.rb
+++ fetch-data.rb
@@ -12,7 +12,7 @@ instances = {}
 
 DB[:clouds].all.each do |cloud|
   cloud_name = cloud[:name]
-  puts "Scanning: #{cloud_name}"
+  puts "cloud=#{cloud_name} at=starting"
 
   azmap = {}
   DB[:availability_zones].where(:cloud_id => cloud[:id]).map do |row|
@@ -23,53 +23,36 @@ DB[:clouds].all.each do |cloud|
     :aws_access_key_id => cloud[:access_key_id],
     :aws_secret_access_key => cloud[:secret_access_key]})
 
-    compute.describe_reserved_instances.body["reservedInstancesSet"].map do |ri|
-      DB[:reservations].insert(
-        :id => ri["reservedInstancesId"],
-        :cloud_id => cloud[:id],
-        :availability_zone => ri["availabilityZone"],
-        :instance_type => ri["instanceType"],
-        :instance_count => ri["instanceCount"],
-        :start => ri["start"],
-        :duration_seconds => ri["duration"],
-        :offering_type => ri["offeringType"],
-      )
-    end
-
-    compute.servers.map do |i|
-      if azmap.key? i.availability_zone
-        az = azmap[i.availability_zone]
-      else
-        key = "#{cloud_name}/#{i.availability_zone}"
-        puts "Warning: Unknown AZ: #{cloud_name}/#{i.availability_zone}" unless unknown_azs.key? key
-        unknown_azs[key] = true
-        az = key
-      end
-        
-      instances[az] ||= {}
-      instances[az][i.flavor_id] ||= 0
-      instances[az][i.flavor_id] += 1
-      now = Time.now
-      DB[:running_instances].insert(
-        :cloud_id => cloud[:id],
-        :availability_zone => i.availability_zone,
-        :instance_type => i.flavor_id,
-        :created_at => i.created_at,
-        :seen => now,
-      )
-    end
-end
-
-reservations.unshift ["Instance type", "Availability zone", "Instance count", "Start", "Duration (seconds)"]
-reservations_csv = reservations.map {|row| row.to_csv}.join
-
-instances_table = [["Instance type", "Availability zone", "Instance count"]]
-instances.each do |availability_zone, hash|
-  hash.each do |instance_type, count|
-    instances_table << [availability_zone, instance_type, count]
+  num_reserved_instances = 0
+  compute.describe_reserved_instances.body["reservedInstancesSet"].map do |ri|
+    DB[:reservations].insert(
+      :id => ri["reservedInstancesId"],
+      :cloud_id => cloud[:id],
+      :availability_zone => ri["availabilityZone"],
+      :instance_type => ri["instanceType"],
+      :instance_count => ri["instanceCount"],
+      :start => ri["start"],
+      :duration_seconds => ri["duration"],
+      :offering_type => ri["offeringType"],
+    )
+    num_reserved_instances += 1
   end
+
+  num_servers = 0
+  compute.servers.map do |i|
+    now = Time.now
+    DB[:running_instances].insert(
+      :cloud_id => cloud[:id],
+      :availability_zone => i.availability_zone,
+      :instance_type => i.flavor_id,
+      :created_at => i.created_at,
+      :seen => now,
+    )
+    num_servers += 1
+  end
+
+  puts "cloud=#{cloud_name} at=finished num_servers=#{num_servers} num_reserved_instances=#{num_reserved_instances}"
 end
-instances_csv = instances_table.map {|row| row.to_csv}.join
 
 timestamp = Time::now.to_s
 
