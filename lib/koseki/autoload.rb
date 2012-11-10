13f3e36935be6055e70697939a60cfa36fb447e4
Matt Zimmerman <mdz@singly.com>
2012-11-10 10:05:45 -0800

    Convert availability_zone_mapping to a Model

--- lib/koseki/autoload.rb
+++ lib/koseki/autoload.rb
@@ -2,4 +2,5 @@ module Koseki
   autoload :Cloud, "koseki/cloud"
   autoload :Reservation, "koseki/reservation"
   autoload :AvailabilityZone, "koseki/availability_zone"
+  autoload :AvailabilityZoneMapping, "koseki/availability_zone_mapping"
 end
