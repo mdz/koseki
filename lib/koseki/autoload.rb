2a920cf34c35ecc91b48aeeaff32464b2ff3eba2
Matt Zimmerman <mdz@singly.com>
2012-11-10 10:14:20 -0800

    Convert instances to a Model

--- lib/koseki/autoload.rb
+++ lib/koseki/autoload.rb
@@ -3,4 +3,5 @@ module Koseki
   autoload :Reservation, "koseki/reservation"
   autoload :AvailabilityZone, "koseki/availability_zone"
   autoload :AvailabilityZoneMapping, "koseki/availability_zone_mapping"
+  autoload :Instance, "koseki/instance"
 end
