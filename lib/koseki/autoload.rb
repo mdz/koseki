c44afe4c46496a9fa8d4d3803522d60cd89509eb
Matt Zimmerman <mdz@singly.com>
2012-11-10 18:29:26 -0800

    Import reserved instance prices

--- lib/koseki/autoload.rb
+++ lib/koseki/autoload.rb
@@ -4,4 +4,5 @@ module Koseki
   autoload :AvailabilityZone, "koseki/availability_zone"
   autoload :AvailabilityZoneMapping, "koseki/availability_zone_mapping"
   autoload :Instance, "koseki/instance"
+  autoload :InstanceReservedPrice, "koseki/instance_reserved_price"
 end
