fbc436b5aa12eb4aba1fb9fe4b7991a12e10ed31
Matt Zimmerman <mdz@singly.com>
2012-11-19 20:57:35 -0800

    Use up instead of change

--- migrations/1_normalize_az.rb
+++ migrations/1_normalize_az.rb
@@ -1,5 +1,5 @@
 Sequel.migration do
-  change do
+  up do
     alter_table(:availability_zone_mappings) do
       rename_column :availability_zone_id, :az_id
     end
@@ -13,9 +13,9 @@ Sequel.migration do
       rename_column :availability_zone, :logical_az
       add_foreign_key :az_id, :availability_zones
     end
-  end
-  up do
+
     self["UPDATE reservations SET az_id = availability_zone_mappings.az_id FROM availability_zone_mappings WHERE availability_zone_mappings.cloud_id = reservations.cloud_id AND availability_zone_mappings.logical_az = reservations.logical_az"]
     self["UPDATE instances SET az_id = availability_zone_mappings.az_id FROM availability_zone_mappings WHERE availability_zone_mappings.cloud_id = instances.cloud_id AND availability_zone_mappings.logical_az = instances.logical_az"]
+
   end
 end
