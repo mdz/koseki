ad037bec25aa7cd4c8a7b97cc1848ea024609964
Matt Zimmerman <mdz@singly.com>
2012-11-22 12:20:11 -0800

    Get EBS volumes for prod too

--- lib/koseki/cloud.rb
+++ lib/koseki/cloud.rb
@@ -30,7 +30,7 @@ module Koseki
         refresh_availability_zones
         refresh_reserved_instances
         refresh_instances
-        if @cloud.name == 'shogun'
+        if @cloud.name == 'shogun' or @cloud.name == 'us-east-1'
           # XXX remove this guard once we can do DescribeVolumes for all clouds
           refresh_volumes
         end
