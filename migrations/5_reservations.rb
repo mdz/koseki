cc40c6b1e1a86f23999b531ec18da037338cf401
Matt Zimmerman <mdz@heroku.com>
2012-12-14 14:25:24 -0800

    Fix quoting

--- migrations/5_reservations.rb
+++ migrations/5_reservations.rb
@@ -5,5 +5,5 @@ Sequel.migration do
       rename_column :start, :start_time
       rename_column :duration_seconds, :duration
     end
-    DB.run('update reservations set end_time = start_time + duration*'1 second'::interval')
+    DB.run("update reservations set end_time = start_time + duration*'1 second'::interval")
 end
