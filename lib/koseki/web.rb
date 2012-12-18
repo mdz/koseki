89946c7aa0e6c477b02da990645d679b5e5c4270
Matt Zimmerman <mdz@heroku.com>
2012-12-18 15:00:52 -0800

    Add HTTP basic auth

--- lib/koseki/web.rb
+++ lib/koseki/web.rb
@@ -3,8 +3,25 @@ require 'json'
 
 module Koseki
   class Web < Sinatra::Base
+    helpers do
+      raise "No API_KEY specified" unless ENV['API_KEY']
+
+      def protected_by_api_key!
+        unless http_basic_authorized?
+          response['WWW-Authenticate'] = %(Basic realm="Restricted Area")
+          throw(:halt, [401, "Not authorized\n"])
+        end
+      end
+
+      def http_basic_authorized?
+        return false unless ENV['API_KEY']
+        @auth ||=  Rack::Auth::Basic::Request.new(request.env)
+        @auth.provided? && @auth.basic? && @auth.credentials && ENV['API_KEY'] && @auth.credentials[1] == ENV['API_KEY']
+      end
+    end
 
     post "/register-cloud" do
+      protected_by_api_key!
       if not Koseki::Cloud.where(Sequel.or({:name => params['name'], :account_number => params['account_number']})).empty?
         status 400
         return JSON.dump({"error" => "A cloud already exists with the specified name or account number"})
