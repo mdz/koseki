b67b7a774cc7f18b451505d7a3da5f2a828974a3
Matt Zimmerman <mdz@heroku.com>
2012-12-18 15:24:18 -0800

    Remove redundant check

--- lib/koseki/web.rb
+++ lib/koseki/web.rb
@@ -15,7 +15,6 @@ module Koseki
       end
 
       def http_basic_authorized?
-        return false unless ENV['API_KEY']
         @auth ||=  Rack::Auth::Basic::Request.new(request.env)
         @auth.provided? && @auth.basic? && @auth.credentials && ENV['API_KEY'] && @auth.credentials[1] == ENV['API_KEY']
       end
