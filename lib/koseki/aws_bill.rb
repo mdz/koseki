be2f98761ea01c4c19c791ef8f2bdfcded66bf18
Matt Zimmerman <mdz@heroku.com>
2013-03-12 22:15:28 -0700

    Refactor programmating billing import, add support for detailed line items

--- lib/koseki/aws_bill.rb
+++ lib/koseki/aws_bill.rb
@@ -1,28 +1,98 @@
+require 'koseki/aws_bill_line_item.rb'
+require 'zip'
+
 module Koseki
   class AWSBill < Sequel::Model
-    def self.refresh_from_csv_in_s3(cloud, object)
-      puts "cloud=#{cloud.name} fn=refresh_from_csv_in_s3 object=#{object.key} at=start"
+    def self.import_s3_object(cloud, object)
+      import_file(cloud, object.key, object.body, object.last_modified)
+    end
+
+    def self.import_file(cloud, filename, contents, last_modified)
+      puts "cloud=#{cloud.name} fn=import_file filename=#{filename} at=start"
+
+      if filename.end_with? '.zip'
+        temp = Tempfile.open self.class.name
+        begin
+          temp.write(contents)
+          temp.close
+          Zip::ZipFile.open(temp.path) do |zipfile|
+            for entry in zipfile.entries
+              import_file(cloud, entry.name, entry.get_input_stream, entry.time)
+            end
+          end
+        ensure
+          temp.close!
+        end
+        return
+      end
+      
+      fields = parse_name(filename)
+      if !fields
+        puts "cloud=#{cloud.name} fn=import_file filename=#{filename} at=skip"
+        return
+      end
+
       db.transaction do
         already_existed = true
-        bill = AWSBill.find_or_create(:cloud_id => cloud.id, :name => object.key) do |bill|
+        bill = AWSBill.find_or_create(:cloud_id => cloud.id, :name => filename) do |bill|
           bill.cloud_id = cloud.id
-          bill.name = object.key
-          bill.last_modified = object.last_modified
+          bill.name = filename
+          bill.last_modified = last_modified
+          parse_name(filename).each do |key, value|
+            if bill.respond_to? key
+              bill.send((key+'=').to_sym, value)
+            end
+          end
           already_existed = false
         end
 
         if already_existed
-          fresh = (bill.last_modified == object.last_modified)
-          puts "cloud=#{cloud.name} fn=refresh_from_csv_in_s3 object=#{object.key} at=fresh current=#{bill.last_modified} new=#{object.last_modified} fresh=#{fresh}"
+          fresh = (bill.last_modified == last_modified)
+          puts "cloud=#{cloud.name} fn=import_file filename=#{filename} at=fresh current=#{bill.last_modified} new=#{last_modified} fresh=#{fresh}"
           return if fresh
         end
 
-        expired_records = Koseki::AWSBillLineItem.purge(bill)
-        new_records = Koseki::AWSBillLineItem.import_csv(bill, object.body)
-
-        bill.update(:last_modified => object.last_modified)
-        puts "cloud=#{cloud.name} fn=refresh_from_csv_in_s3 at=finish object=#{object.key} new_records=#{new_records} expired_records=#{expired_records}"
+        old_records, new_records = bill.import_data(fields['format'], contents)
+        bill.update(:last_modified => last_modified)
+        puts "cloud=#{cloud.name} fn=import_file at=finish new_records=#{new_records} old_records=#{old_records}"
       end
+
+    end
+
+    def self.parse_name(name)
+      re = /^(?<account_number>\d+)-aws-(?<type>.*)-(?<year>\d\d\d\d)-(?<month>\d\d)\.(?<format>.*)$/
+      match = re.match(name)
+      return nil unless match
+
+      fields = Hash[match.names.zip(match.captures)]
+      fields['month_start'] = Time.mktime(fields['year'].to_i, fields['month'].to_i)
+      return fields
+    end
+
+    def import_data(format, body)
+      if format == 'csv'
+        if type == 'billing-csv' or type == 'cost-allocation' or type == 'billing-detailed-line-items'
+          return replace_line_items(body)
+        else
+          raise "Unknown bill type: #{type}"
+        end
+      else
+        raise "Unknown data format: #{format}"
+      end
+    end
+
+    def purge
+      records = Koseki::AWSBillLineItem.where(:aws_bill_id => id)
+      record_count = records.count
+      records.delete
+      return record_count
+    end
+
+
+    def replace_line_items(body)
+      old_records = purge
+      new_records = Koseki::AWSBillLineItem.import_csv(self, body)
+      return old_records, new_records
     end
   end
 end
--- lib/koseki/aws_bill_line_item.rb
+++ lib/koseki/aws_bill_line_item.rb
@@ -1,58 +1,69 @@
+require 'koseki/aws_line_item_base'
+
 module Koseki
   class AWSBillLineItem < Sequel::Model
     def self.import_csv(bill, csv)
+      puts "fn=import_csv at=start"
+      start_time = Time.now
+
       field_names = []
+      column_names = []
       line_number = 0
-      has_tags = false
       new_records = 0
+      insert_batch = []
 
       CSV.parse(csv) do |row|
-        line_number += 1
         if row[0] == 'InvoiceID'
           field_names = row
-          has_tags = field_names.grep('^user:')
+          column_names = field_names.map { |field_name|
+            field_name.gsub(/::/, '/').
+                gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
+                gsub(/([a-z\d])([A-Z])/,'\1_\2').
+                tr("-", "_").
+                downcase
+          }
           next
         elsif row.length <= 1
           # first line of cost allocation report is a comment
           next
         end
 
-        fields = Hash[field_names.zip(row)]
-
-        line = self.create do |line|
-          new_records += 1
-          line.aws_bill_id = bill.id
-          line.line_number = line_number
-          if has_tags
-            line.tags = Sequel::Postgres::HStore.new([])
-          end
-
-          fields.each do |key, value|
-            if key.start_with? "user:"
-              # store user tags in the tags column
-              tag_name = key.slice(/:(.*)$/, 1)
-              line.tags[tag_name] = value
-            else
-              # convert CSV column heading into database column name
-              column_name = key.gsub(/::/, '/').
-                gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
-                gsub(/([a-z\d])([A-Z])/,'\1_\2').
-                tr("-", "_").
-                downcase
-              line.send((column_name+'=').to_sym, value)
-            end
+        record = {}
+        record[:aws_bill_id] = bill.id
+        record['line_number'] = line_number
+        row.each_with_index do |value, index|
+          field_name = field_names[index]
+          if field_name.start_with? "user:"
+            record['tags'] ||= Sequel::Postgres::HStore.new([])
+            tag_name = field_name.slice(/:(.*)$/, 1)
+            record['tags'][tag_name] = value
+          else
+            record[column_names[index]] = value
           end
         end
+        insert_batch << record
+
+        if insert_batch.length >= 1000
+          elapsed = Time.now-start_time
+          self.multi_insert(insert_batch)
+          new_records += insert_batch.length
+          insert_batch = []
+
+          puts "fn=import_csv at=flush new_records=#{new_records} elapsed=#{elapsed.round} records_per_second=#{new_records/elapsed.round}" unless elapsed.round == 0
+        end
+
+        line_number += 1
+      end
+      # flush any remaining inserts
+      if !insert_batch.empty?
+        self.multi_insert(insert_batch)
+        new_records += insert_batch.length
       end
 
+      elapsed = Time.now-start_time
+      puts "fn=import_csv at=finish new_records=#{new_records} elapsed=#{elapsed.round} records_per_second=#{new_records/elapsed}"
       return new_records
     end
 
-    def self.purge(bill)
-      records = self.where(:aws_bill_id => bill.id)
-      record_count = expired_records.count
-      records.delete
-      return record_count
-    end
   end
 end
